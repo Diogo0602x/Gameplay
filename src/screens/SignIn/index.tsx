679f31dfb267d87e38b5291afde0fb6024d089ed
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-21 22:33:56 -0300

    Aula 1 terminada

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, Text, Image, StatusBar} from 'react-native'
+
+import { styles } from './styles';
+import { ButtonIcon } from '../../components/Buttonicon';
+import IllustrationImg from '../../assets/illustration.png';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image 
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organiza {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text> 
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com DIscord"
+          activeOpacity={0.7}
+        />
+      </View>
+    </View>
+  );
+}
