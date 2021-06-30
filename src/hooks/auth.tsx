02875dc3f4f59df4692a9cf1b9df3b29300c1345
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-30 19:58:08 -0300

    Aula 5 terminada

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,22 +1,23 @@
-import React, 
-{ 
+import React,
+{
   createContext,
   useContext,
   useState,
-  ReactNode
+  ReactNode,
+  useEffect,
 } from 'react';
 
 import * as AuthSession from 'expo-auth-session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import {
-  SCOPE,
-  CLIENT_ID,
-  CDN_IMAGE,
-  REDIRECT_URI,
-  RESPONSE_TYPE
-} from '../configs';
+const { SCOPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
+const { REDIRECT_URI } = process.env;
+const { RESPONSE_TYPE } = process.env;
 
 import { api } from '../services/api';
+import { COLLECTION_USERS } from '../configs/database';
 
 type User = {
   id: string;
@@ -31,6 +32,7 @@ type AuthContextData = {
   user: User;
   loading: boolean;
   signIn: () => Promise<void>;
+  singOut: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -39,7 +41,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
-    access_token: string;
+    access_token?: string;
+    error?: string;
   }
 }
 
@@ -53,40 +56,63 @@ function AuthProvider({ children }: AuthProviderProps) {
     try {
       setLoading(true);
 
-      const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`; 
+      const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
       const { type, params } = await AuthSession
-      .startAsync({ authUrl }) as AuthorizationResponse;
+        .startAsync({ authUrl }) as AuthorizationResponse;
 
-      if(type === "success"){ 
+      if (type === "success" && !params.error) {
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
         const userInfo = await api.get('/users/@me');
 
         const firstName = userInfo.data.username.split(' ')[0];
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
-       
-        setUser({
+
+        const userData = {
           ...userInfo.data,
           firstName,
           token: params.access_token
-        });
-        setLoading(false);
-      }else{
-        setLoading(false);
+        }
+
+        await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+        setUser(userData);
       }
     } catch {
       throw new Error('Não foi possível autenticar');
+    } finally {
+      setLoading(false);
     }
   }
+
+  async function singOut() {
+    setUser({} as User);
+    await AsyncStorage.removeItem(COLLECTION_USERS);
+  }
+
+  async function loadUserStorageData() {
+    const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+
+    if (storage) {
+      const userLogged = JSON.parse(storage) as User;
+      api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, []);
 
   return (
     <AuthContext.Provider value={{
       user,
       loading,
-      signIn
+      signIn,
+      singOut
     }}>
-      { children }
+      {children}
     </AuthContext.Provider>
   )
 }
--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,47 +1,95 @@
-import React from 'react';
-import { Fontisto } from '@expo/vector-icons';
+import React, { useState, useEffect } from 'react';
 import { BorderlessButton } from 'react-native-gesture-handler';
+import { useRoute } from '@react-navigation/native';
+import { Fontisto } from '@expo/vector-icons';
+import * as Linking from 'expo-linking';
 
 import {
   ImageBackground,
   Text,
   View,
-  FlatList
+  Alert,
+  FlatList,
+  Share,
+  Platform
 } from 'react-native';
 
-import { theme } from '../../global/styles/theme';
-import { styles } from './styles';
 import BannerImg from '../../assets/banner.png';
 
+import { theme } from '../../global/styles/theme';
+import { api } from '../../services/api';
+import { styles } from './styles';
+
+import { AppointmentProps } from '../../components/Appointment';
+import { Member, MemberProps } from '../../components/Member';
 import { ListDivider } from '../../components/ListDivider';
 import { Background } from '../../components/Background';
 import { ListHeader } from '../../components/ListHeader';
 import { ButtonIcon } from '../../components/ButtonIcon';
-import { Member } from '../../components/Member';
 import { Header } from '../../components/Header';
+import { Load } from '../../components/Load';
 
-export function AppointmentDetails(){
-  const members = [
-    {
-      id: '1',
-      username: 'Rodrigo',
-      avatar_url: 'https://github.com/rodrigorgtic.png',
-      status: 'online'
-    },
-    {
-      id: '2',
-      username: 'Rodrigo',
-      avatar_url: 'https://github.com/rodrigorgtic.png',
-      status: 'offline'
+type Params = {
+  guildSelected: AppointmentProps
+}
+
+type GuildWidget = {
+  id: string;
+  name: string;
+  instant_invite: string;
+  members: MemberProps[];
+}
+
+export function AppointmentDetails() {
+  const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget);
+  const [loading, setLoading] = useState(true);
+
+  const route = useRoute();
+  const { guildSelected } = route.params as Params;
+
+  async function fetchGuildWidget() {
+    try {
+      const response = await api.get(`/guilds/${guildSelected.guild.id}/widget.json`);
+      setWidget(response.data);
+    } catch {
+      /* 
+        UPDATE 5# 
+        Observação: Além de ativar o widget do servidor, escolha um canal pra convite pois por padrão 
+        estava como sem canal e, dessa forma, o instant_invite pode vir como null mesmo para o servidor. 
+        Setando um canal qualquer e resetando a aplicação resolve o problema do instant_invite ir como null.
+      */
+      Alert.alert('Verifique as configurações do servidor. Será que o Widget está habilitado?');
+    } finally {
+      setLoading(false);
     }
-  ]
+  }
+
+  function handleShareInvitation() {
+    const message = Platform.OS === 'ios'
+      ? `Junte-se a ${guildSelected.guild.name}`
+      : widget.instant_invite;
+
+    Share.share({
+      message,
+      url: widget.instant_invite
+    });
+  }
+
+  function handleOpenGuild() {
+    Linking.openURL(widget.instant_invite);
+  }
+
+  useEffect(() => {
+    fetchGuildWidget();
+  }, []);
   return (
     <Background>
-      <Header 
+      <Header
         title="Detalhes"
         action={
-          <BorderlessButton>
-            <Fontisto 
+          guildSelected.guild.owner &&
+          <BorderlessButton onPress={handleShareInvitation}>
+            <Fontisto
               name="share"
               size={24}
               color={theme.colors.primary}
@@ -50,39 +98,58 @@ export function AppointmentDetails(){
         }
       />
 
-      <ImageBackground 
+      <ImageBackground
         source={BannerImg}
         style={styles.banner}
       >
         <View style={styles.bannerContent}>
-          <Text style={styles.title}> 
-            Lendários 
+          <Text style={styles.title}>
+            {guildSelected.guild.name}
           </Text>
 
           <Text style={styles.subtitle}>
-            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+            {guildSelected.description}
           </Text>
         </View>
       </ImageBackground>
 
-      <ListHeader 
-        title="Jogadores"
-        subtitle="Total 3"
-      />
+      {
+        loading ? <Load /> :
+          <>
+            <ListHeader
+              title="Jogadores"
+              subtitle={`Total ${widget.members.length ? widget.members.length : 0}`}
+            />
+            {
+              <FlatList
+                data={widget.members ? widget.members : []} // UPDATE 1# Bom cuidar aqui, caso não encontre um Widget automaticamente não haverá membros.
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                  <Member data={item} />
+                )}
+                ItemSeparatorComponent={() => <ListDivider isCentered />}
+                style={styles.members}
+                ListEmptyComponent={() => ( // UPDATE 2#  Um propriedade para renderizar algo quando a lista e vázia.
+                  <View style={styles.emptyContainer}>
+                    <Text style={styles.emptyText}>
+                      Não há ninguém online agora.
+                    </Text>
+                  </View>
+                )}
+              />
+            }
+          </>
+      }
 
-      <FlatList 
-        data={members}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <Member data={item} />
-        )}
-        ItemSeparatorComponent={() => <ListDivider />}
-        style={styles.members}
-      />
-
-      <View style={styles.footer}>
-        <ButtonIcon title="Entrar na partida" />
-      </View>
+      {
+        guildSelected.guild.owner &&
+        <View style={styles.footer}>
+          <ButtonIcon
+            title="Entrar na partida"
+            onPress={handleOpenGuild}
+          />
+        </View>
+      }
     </Background>
   );
 }
